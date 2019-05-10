83874c6739d4f1049e24cc75a3d6793cdf134277
soaresandrie <jsoaresandrie@gmail.com>
2019-05-10 20:59:45 -0300

    Começo de tudo.

---
+++ index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const server = express();
+const { HOST, PORT } = process.env;
+
+server.get("/paypal", PayPalController.request);
+
+server.listen(PORT, () => {
+    console.log(`Servidor escutando em http://${HOST}:${PORT}`);
+});
